8503c246b432f8f058dd6ff1f713a74a0243f790
tanimtorikul <torikulswe@gmail.com>
2023-11-27 13:26:40 +0600

    implemented packages api, api based on id and api based on tourtype query

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.o2yungw.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -25,6 +25,27 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const packageCollection = client.db("gotripDB").collection("packages");
+    // package related apis
+    app.get("/packages", async (req, res) => {
+      const result = await packageCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/packages/:id", async (req, res) => {
+      const packageId = req.params.id;
+      const objectId = new ObjectId(packageId);
+      const result = await packageCollection.findOne({ _id: objectId });
+      res.send(result);
+    });
+    app.get("/packages/tourType/:tourType", async (req, res) => {
+      const tourType = req.params.tourType;
+      const query = { tourType };
+      const result = await packageCollection.find(query).toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
