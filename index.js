16da94baa1bddc4abfd39c2a75ff60db12971269
tanimtorikul <torikulswe@gmail.com>
2023-12-02 12:20:58 +0600

    made an api to get admin only

--- index.js
+++ index.js
@@ -47,14 +47,14 @@ async function run() {
       if (!req.headers.authorization) {
         return res.status(401).send({ message: "forbidden access" });
       }
-      const token = req.headers.authorization.split(' ')[1]
+      const token = req.headers.authorization.split(" ")[1];
       jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
         if (err) {
-          return res.status(401).send({message: 'forbidden access'})
+          return res.status(401).send({ message: "forbidden access" });
         }
         req.decoded = decoded;
         next();
-      })
+      });
     };
 
     // package related apis
@@ -111,6 +111,19 @@ async function run() {
       };
       const result = await userCollection.updateOne(filter, updatedDoc);
       res.send(result);
+    });
+    app.get("/users/admin/:email", verifyToken, async (req, res) => {
+      const email = req.params.email;
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: "unauthorized access" });
+      }
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === "admin";
+      }
+      res.send({ admin });
     });
 
     // stories related apis
